289fa7c684db47f58b3e3e97e57da2ded0247fe4
Idontknow13 <curtmatthewgarcia@gmail.com>
2026-02-08 23:08:43 +0800

    backend-feat: Changed `main.py` to `server.py` and changed `app` to `api`

--- cheshire-backend/server.py
+++ cheshire-backend/server.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+api = FastAPI()
 
-@app.get("/hello_world")
+@api.get("/hello_world")
 def hello_world():
     return {"Hello": "World"}
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, port=8000)
+    uvicorn.run(api, port=8000)
