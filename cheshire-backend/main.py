91dab420dfaf63e4133dde807d8e8b2328df0216
Idontknow13 <curtmatthewgarcia@gmail.com>
2026-02-08 20:01:03 +0800

    feat: Implement a basic FastAPI application with a hello world endpoint and add its dependencies.

--- cheshire-backend/main.py
+++ cheshire-backend/main.py
@@ -1,5 +1,11 @@
-def main() -> None:
-    print("Hello World")
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/hello_world")
+def hello_world():
+    return {"Hello": "World"}
 
 if __name__ == "__main__":
-    main()
+    import uvicorn
+    uvicorn.run(app, port=8000)
