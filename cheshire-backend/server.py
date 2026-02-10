3c2d61b3c0a1b422de506b649eafab805035a129
yohan21-dev <ivan.maano21@gmail.com>
2026-02-10 18:32:23 +0000

    Added healthcheck and hello_world endpoints

--- cheshire-backend/server.py
+++ cheshire-backend/server.py
@@ -2,10 +2,23 @@ from fastapi import FastAPI
 
 api = FastAPI()
 
+
+@api.get("/")
+def read_root() -> dict:
+    return {"message": "Welcome to the Cheshire Backend API!"}
+
+
 @api.get("/hello_world")
-def hello_world():
+def hello_world() -> dict:
     return {"Hello": "World"}
+
+
+@api.get("/hello/{name}")
+def hello_name(name: str) -> dict:
+    return {"Hello": name}
+
 
 if __name__ == "__main__":
     import uvicorn
+
     uvicorn.run(api, port=8000)
