6f8d223b3eb0008b83c93921802fcd49bf90331e
sleeptical <yumidocusin@gmail.com>
2026-02-13 03:34:38 +0800

    feat: Add upload card and new chat on sidebar

--- cheshire-frontend/src/App.tsx
+++ cheshire-frontend/src/App.tsx
@@ -1,4 +1,7 @@
+import { Button } from "@/components/ui/button";
+import { Upload } from "lucide-react";
 import CheshireSidebar from "./components/cheshire-sidebar";
+import { Card, CardAction, CardContent } from "./components/ui/card";
 import { SidebarProvider, SidebarTrigger, SidebarInset } from "./components/ui/sidebar";
 
 export function App() {
@@ -9,8 +12,18 @@ export function App() {
                 <SidebarTrigger />
                 <SidebarInset>
                     <main>
-                        {/* all content goes here */}
-                        
+                        <Card className="w-full max-w-2xl m-50 mx-50">
+                            <CardContent/>
+                               <CardContent>
+                               <p className="text-center font-bold text-xl">Hi! Welcome to Cheshire. Please upload a document to evaluate. Thank you!</p>
+                               </CardContent>
+                                <CardAction>
+                                    <Button type="submit" className="mx-65 w-40">
+                                        <Upload />
+                                        Upload
+                                    </Button>
+                                </CardAction>
+                        </Card>
                     </main>
                 </SidebarInset>
             </SidebarProvider>
--- cheshire-frontend/src/components/cheshire-sidebar.tsx
+++ cheshire-frontend/src/components/cheshire-sidebar.tsx
@@ -8,28 +8,32 @@ import {
 } from "@/components/ui/sidebar";
 import {
     CircleUserRound,
-    Settings
+    Settings,
+    SquarePen
 } from "lucide-react";
 import AccountDetails from "./account-details";
-
 
 export function CheshireSidebar() {
     return (
         <Sidebar>
-            <SidebarHeader className="p-5 pt-6">
-                <SidebarMenu>
-                    <SidebarMenuItem className="flex items-start gap-1 border-b">
+            <SidebarHeader className="p-5 pt-6 border-b">
+                <SidebarMenu className="flex flex-col gap-4">
+                    <SidebarMenuItem className="flex flex-row items-start gap-1 m-2">
                         <CircleUserRound />
                         <AccountDetails />
+                    </SidebarMenuItem>
+                    <SidebarMenuItem className="flex flex-row gap-2">
+                        <SquarePen />
+                        New Chat 
                     </SidebarMenuItem>
                 </SidebarMenu>
             </SidebarHeader>
             <SidebarContent>
                 
             </SidebarContent>
-            <SidebarFooter className="flex flex-row p-5">
+            <SidebarFooter className="flex flex-row p-5 border-t">
                 <Settings />
-                Setting
+                Settings 
             </SidebarFooter>
         </Sidebar>
     );
