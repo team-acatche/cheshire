1ab4f71ebe7ea554e3b12c7772312a3fa7f92042
sleeptical <yumidocusin@gmail.com>
2026-02-13 02:14:42 +0800

    feat: added sidebar

--- cheshire-frontend/src/App.tsx
+++ cheshire-frontend/src/App.tsx
@@ -1,7 +1,21 @@
-import { ComponentExample } from "@/components/component-example";
+import CheshireSidebar from "./components/cheshire-sidebar";
+import { SidebarProvider, SidebarTrigger, SidebarInset } from "./components/ui/sidebar";
 
 export function App() {
-return <ComponentExample />;
+    return (
+        <>
+            <SidebarProvider>
+                <CheshireSidebar />
+                <SidebarTrigger />
+                <SidebarInset>
+                    <main>
+                        {/* all content goes here */}
+                        
+                    </main>
+                </SidebarInset>
+            </SidebarProvider>
+        </>
+    );
 }
 
 export default App;
