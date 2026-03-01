633642481b70065f88b77c6b18b1b4908ab88d3d
Idontknow13 <curtmatthewgarcia@gmail.com>
2026-03-01 23:20:37 +0800

    feat: Migrate to Haystack AI, add output schemas, and introduce new tools for web search, query, and vulnerability analysis.

---
+++ cheshire-backend/tools/exa.py
@@ -0,0 +1,25 @@
+from haystack_integrations.components.websearch.exa import ExaAnswer
+from haystack.tools.component_tool import ComponentTool
+
+from tools.helpers.document_to_string import document_to_string
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+web_search = ComponentTool(
+	component=ExaAnswer(),
+	name="web_search",
+	description="Answer questions through the web using Exa search.",
+	parameters={
+		"type": "object",
+		"properties": {
+			"query": {
+				"type": "string",
+				"description": "The question to answer.",
+			},
+		},
+		"required": ["query"],
+	},
+	outputs_to_string={"source": "answer", "handler": str}
+)
