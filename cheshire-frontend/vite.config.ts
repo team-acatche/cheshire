63344df6e0466810b958e6af6eff7e1c2dc0dd3a
sleeptical <yumidocusin@gmail.com>
2026-03-29 22:31:54 +0800

    merge: get updated dependencies and Dockerfile from main

--- cheshire-frontend/vite.config.ts
+++ cheshire-frontend/vite.config.ts
@@ -3,8 +3,18 @@ import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
-// https://vite.dev/config/
 export default defineConfig({
+  server: {
+    allowedHosts: true,
+    host: true,
+    port: 5173,
+    proxy: {
+      "/api": {
+        target: process.env.BACKEND_URI ?? "http://localhost:8000",
+        changeOrigin: true,
+      },
+    },
+  },
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
