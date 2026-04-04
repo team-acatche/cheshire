d49ca964a6c366303972dd47154d1d14bbf0b9aa
Idontknow13 <curtmatthewgarcia@gmail.com>
2026-04-05 00:12:49 +0800

    feat: fixed issue where FastEmbed tries to run embedding models on the GPU if there are visible CUDA devices

--- cheshire-backend/src/cheshire_configs/ollama.py
+++ cheshire-backend/src/cheshire_configs/ollama.py
@@ -1,15 +1,15 @@
 import os
 
 from haystack_integrations.components.generators.ollama import OllamaChatGenerator
-# from haystack_integrations.components.embedders.ollama import OllamaTextEmbedder
-# from haystack_integrations.components.embedders.ollama import OllamaDocumentEmbedder
-
 from haystack_integrations.components.embedders.fastembed import FastembedTextEmbedder, FastembedDocumentEmbedder
 
 from cheshire_configs.core import DefaultToolFactory, PipelineConfig
 
 
 async def ollama_config() -> PipelineConfig:
+    # Force fastembed (onnxruntime) to use CPU to avoid NVRTC/CUDA initialization crashes
+    os.environ["CUDA_VISIBLE_DEVICES"] = ""
+
     # TODO: remove this once the user can supply their own config
     from dotenv import load_dotenv
     load_dotenv(".env.user")
@@ -23,14 +23,6 @@ async def ollama_config() -> PipelineConfig:
             },
             think=True,
         ),
-        # document_embedder=lambda: OllamaDocumentEmbedder(
-        #     model=os.getenv("OLLAMA_EMBEDDING_MODEL", "nomic-embed-text"),
-        #     url=os.getenv("OLLAMA_URL", "http://localhost:11434"),
-        # ),
-        # embedder=lambda: OllamaTextEmbedder(
-        #     model=os.getenv("OLLAMA_EMBEDDING_MODEL", "nomic-embed-text"),
-        #     url=os.getenv("OLLAMA_URL", "http://localhost:11434"),
-        # ),
         document_embedder=lambda: FastembedDocumentEmbedder(model="sentence-transformers/all-MiniLM-L6-v2"),
         embedder=lambda: FastembedTextEmbedder(model="sentence-transformers/all-MiniLM-L6-v2"),
         tools=DefaultToolFactory().tools,
